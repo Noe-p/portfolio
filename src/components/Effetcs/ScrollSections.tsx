eb9a1a156a5bcb55c8e0543ca762c26f9d6f9c1a
Noé <noe.philippe@bob-le-developpeur.com>
2025-04-25 19:24:55 +1200

    🚧 Test Scroll Section 10

--- src/components/Effetcs/ScrollSections.tsx
+++ src/components/Effetcs/ScrollSections.tsx
@@ -77,7 +77,7 @@ export function ScrollSections(): React.JSX.Element {
       ? screenHeight +
         (sections.length - 1) * SPEED * screenHeight +
         screenHeight * SPEED +
-        150
+        100
       : screenHeight +
         (sections.length - 1) * SPEED * screenHeight +
         screenHeight * SPEED;
