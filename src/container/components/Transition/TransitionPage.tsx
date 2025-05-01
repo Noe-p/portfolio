7e63fd3573076cbd350a9f1a5b43727ff37b7e98
Noé <noe.philippe@bob-le-developpeur.com>
2025-05-01 13:11:41 +1200

    ♻️ Refacto Transition page

--- src/container/components/Transition/TransitionPage.tsx
+++ src/container/components/Transition/TransitionPage.tsx
@@ -1,7 +1,9 @@
+'use client';
+
 import { Col } from '@/components';
 import { cn } from '@/services/utils';
-import { motion } from 'framer-motion';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import tw from 'tailwind-styled-components';
 
 interface TransitionPageProps {
@@ -9,26 +11,26 @@ interface TransitionPageProps {
   isEnd?: boolean;
 }
 
-export function TransitionPage(props: TransitionPageProps): JSX.Element {
-  const { className, isEnd = false } = props;
+export function TransitionPage({
+  className,
+  isEnd = false,
+}: TransitionPageProps) {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    if (!isEnd) return;
+    setTimeout(() => {
+      setIsVisible(false);
+    }, 100); // timing à ajuster selon le rendu souhaité
+  }, [isEnd]);
 
   return (
     <Main className={className}>
-      <div className='absolute inset-0 w-full h-full pointer-events-none overflow-hidden z-0'>
-        <div
-          className={cn(
-            'absolute w-[150vw] h-[150vw] rounded-full blur-3xl opacity-30',
-            'md:bg-[radial-gradient(circle,rgba(136,58,255,0.8)_0%,transparent_70%)]',
-            'bg-[radial-gradient(circle,rgba(136,58,255,1)_0%,transparent_100%)]'
-          )}
-        />
-      </div>
-      <motion.div
-        initial={{ opacity: 1 }}
-        animate={{ opacity: isEnd ? 0 : 1 }}
-        exit={{ opacity: isEnd ? 1 : 0 }}
-        transition={{ duration: 0.3, delay: 0.4 }}
-        className='w-full h-full flex items-center justify-center'
+      <Content
+        className={cn(
+          isVisible ? 'opacity-100' : 'opacity-0',
+          'transition-all duration-500 ease-in-out'
+        )}
       >
         <Image
           src='/logo.webP'
@@ -37,9 +39,11 @@ export function TransitionPage(props: TransitionPageProps): JSX.Element {
           alt='logo'
           quality={30}
           priority
-          className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none z-10'
+          className={cn(
+            'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none z-10'
+          )}
         />
-      </motion.div>
+      </Content>
     </Main>
   );
 }
@@ -47,6 +51,15 @@ export function TransitionPage(props: TransitionPageProps): JSX.Element {
 const Main = tw(Col)`
   h-full
   w-full
-  relative
   shadow-lg
+`;
+
+const Content = tw.div`
+  w-full
+  h-full
+  flex
+  items-center
+  justify-center
+  relative
+  z-10
 `;
