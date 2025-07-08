e68713b0973a9d2bace14b6113a8fdede69a0396
Noé <noe.philippe@bob-le-developpeur.com>
2025-07-08 16:53:39 +1200

    💄 Add spotify

--- src/container/pages/HomePage.tsx
+++ src/container/pages/HomePage.tsx
@@ -1,19 +1,21 @@
 'use client';
 
-import { Layout } from '@/components';
+import { Col, Layout, P16, Title } from '@/components';
 import { Separator } from '@/components/ui/separator';
-// import { Spotify } from 'react-spotify-embed';
+import { MenuKeys } from '@/container/components';
+import { useTranslations } from 'next-intl';
+import { Spotify } from 'react-spotify-embed';
 import { Header, ScrollProjects } from '../components';
 
 export function HomePage(): JSX.Element {
-  // const tCommon = useTranslations('common');
+  const tCommon = useTranslations('common');
 
   return (
     <Layout isNavClose={false}>
       <Header />
       <Separator className='mt-10 z-10' />
       <ScrollProjects />
-      {/* <Col id={MenuKeys.MUSIC} className='w-full items-start md:pt-10'>
+      <Col id={MenuKeys.MUSIC} className='w-full items-start md:pt-10'>
         <Col className='md:w-10/12 w-full'>
           <Title>{tCommon('music.title')}</Title>
           <P16 className='text-muted-foreground mt-6'>
@@ -24,11 +26,11 @@ export function HomePage(): JSX.Element {
           <Spotify
             className='w-full mt-10'
             link={
-              'https://open.spotify.com/intl-fr/album/4ubW7rJ7SIXimczwtiBFGU?si=ojmNESb6SHaAtyXIRMG0fA'
+              'https://open.spotify.com/intl-fr/album/1EwDpZ3NaEkcCGyRPEHpPP?si=-2RPwvqpQaKaUizxYDQXlQ'
             }
           />
         </Col>
-      </Col> */}
+      </Col>
     </Layout>
   );
 }
